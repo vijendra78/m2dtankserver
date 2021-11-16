3974c4756c16f8fc1a5a9f729545f17d91fe6abf
Vijendra <kuldeep.singh@ediiie.com>
2021-11-16 11:56:29 +0530

    add server code

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-io = require('socket.io')(process.env.PORT || 52300)
 
-console.log('server has started')
+//Local
+////io = require('socket.io')(process.env.PORT || 52300)
+
+////console.log('server has started')
+
+//Server
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+
+http.listen(process.env.PORT || 8080, function () {
+    console.log('listening on: 8080');
+});
